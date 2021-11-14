1906daf13d0becbfe49f0ca3625d93b404e34275
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-11-14 12:29:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    cin>>n;
+    int n1=0,n2=1,risultato=0;
+    cout<<n2<<endl;
+    while (risultato+n1<=n){
+        risultato=n1+n2;
+        n1=n2;
+        n2=risultato;
+        cout<<risultato<<endl;
+    }
+
+    return 0;
 }
